4c6a9dd61a7d96beac7297eca416f7298eab99e5
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-12-10 21:30:11 +1000

    Solve Day 10 Part 1

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,9 +1,18 @@
+use std::collections::HashSet;
 use std::fs;
 use std::time::Instant;
 
-const PROBLEM_NAME: &str = "###";
+use regex::Regex;
+
+const PROBLEM_NAME: &str = "Cathode-Ray Tube";
 const PROBLEM_INPUT_FILE: &str = "./input/day10.txt";
 const PROBLEM_DAY: u64 = 10;
+
+/// Represents the different instructions for the display CPU.
+enum Instruction {
+    Noop,
+    Addv {value: i64},
+}
 
 /// Processes the AOC 2022 Day 10 input file and solves both parts of the problem. Solutions are
 /// printed to stdout.
@@ -39,22 +48,72 @@ pub fn main() {
 }
 
 /// Processes the AOC 2022 Day 10 input file in the format required by the solver functions.
-/// Returned value is ###.
-fn process_input_file(filename: &str) -> String {
+/// Returned value is vector of Instruction read from the lines of the input file..
+fn process_input_file(filename: &str) -> Vec<Instruction> {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    let regex_noop = Regex::new(r"^noop$").unwrap();
+    let regex_addv = Regex::new(r"^addx (-?\d+)$").unwrap();
+    let mut output: Vec<Instruction> = vec![];
+    for line in raw_input.lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        // Find the instruction type
+        if regex_noop.is_match(line) {
+            output.push(Instruction::Noop);
+        } else if regex_addv.is_match(line) {
+            let caps = regex_addv.captures(line).unwrap();
+            let value = caps[1].parse::<i64>().unwrap();
+            output.push(Instruction::Addv { value });
+        }
+    }
+    output
 }
 
-/// Solves AOC 2022 Day 10 Part 1 // ###
-fn solve_part1(_input: &String) -> String {
-    unimplemented!();
+/// Solves AOC 2022 Day 10 Part 1 // Returns the sum of the signal strengths from the 20th, 60th,
+/// 100th, 140th, 180th and 220th clock cycle.
+fn solve_part1(input: &[Instruction]) -> i64 {
+    // Initialise register
+    let mut reg_x = 1;
+    // Initialise instruction queue
+    let mut target_cycles: HashSet<i64> = HashSet::from([20, 60, 100, 140, 180, 220]);
+    let mut output_sum = 0;
+    let mut clock_cycle = 1;
+    for instruct in input {
+        // Break early if all target cycles have been processed.
+        if target_cycles.is_empty() {
+            break;
+        }
+        // Process the instruction
+        match instruct {
+            Instruction::Noop => {
+                if target_cycles.contains(&clock_cycle) {
+                    output_sum += reg_x * clock_cycle;
+                    target_cycles.remove(&clock_cycle);
+                }
+                clock_cycle += 1;
+            },
+            Instruction::Addv { value } => {
+                for _ in 0..2 {
+                    if target_cycles.contains(&clock_cycle) {
+                        output_sum += reg_x * clock_cycle;
+                        target_cycles.remove(&clock_cycle);
+                    }
+                    clock_cycle += 1;
+                }
+                reg_x += value;
+            }
+        }
+    }
+    output_sum
 }
 
 /// Solves AOC 2022 Day 10 Part 2 // ###
-fn solve_part2(_input: &String) -> String {
-    unimplemented!();
+fn solve_part2(_input: &[Instruction]) -> i64 {
+    0
 }
 
 #[cfg(test)]
@@ -65,9 +124,8 @@ mod test {
     #[test]
     fn test_day10_p1_actual() {
         let input = process_input_file(PROBLEM_INPUT_FILE);
-        let _solution = solve_part1(&input);
-        unimplemented!();
-        // assert_eq!("###", solution);
+        let solution = solve_part1(&input);
+        assert_eq!(16880, solution);
     }
 
     /// Tests the Day 10 Part 2 solver method against the actual problem solution.
