d3bdc38e3982b78440a4c8171817f0c6c5b35162
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-12-11 07:28:41 +1000

    Update day10.rs - Formatting tidy-up and refactor part1 and part2 solutions to   allow reuse of common code segments

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -11,7 +11,7 @@ const PROBLEM_DAY: u64 = 10;
 /// Represents the different instructions for the display CPU.
 enum Instruction {
     Noop,
-    Addv { value: i64 },
+    Addx { value: i64 },
 }
 
 /// Processes the AOC 2022 Day 10 input file and solves both parts of the problem. Solutions are
@@ -54,7 +54,7 @@ fn process_input_file(filename: &str) -> Vec<Instruction> {
     let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
     let regex_noop = Regex::new(r"^noop$").unwrap();
-    let regex_addv = Regex::new(r"^addx (-?\d+)$").unwrap();
+    let regex_addx = Regex::new(r"^addx (-?\d+)$").unwrap();
     let mut output: Vec<Instruction> = vec![];
     for line in raw_input.lines() {
         let line = line.trim();
@@ -64,10 +64,10 @@ fn process_input_file(filename: &str) -> Vec<Instruction> {
         // Find the instruction type
         if regex_noop.is_match(line) {
             output.push(Instruction::Noop);
-        } else if regex_addv.is_match(line) {
-            let caps = regex_addv.captures(line).unwrap();
+        } else if regex_addx.is_match(line) {
+            let caps = regex_addx.captures(line).unwrap();
             let value = caps[1].parse::<i64>().unwrap();
-            output.push(Instruction::Addv { value });
+            output.push(Instruction::Addx { value });
         }
     }
     output
@@ -78,10 +78,9 @@ fn process_input_file(filename: &str) -> Vec<Instruction> {
 fn solve_part1(input: &[Instruction]) -> i64 {
     // Initialise register
     let mut reg_x = 1;
-    // Initialise instruction queue
-    let mut target_cycles: HashSet<i64> = HashSet::from([20, 60, 100, 140, 180, 220]);
-    let mut output_sum = 0;
-    let mut clock_cycle = 1;
+    let mut clock_cycle = 0; // clock cycle 0 is the 1st clock cycle
+    let mut target_cycles: HashSet<i64> = HashSet::from([19, 59, 99, 139, 179, 219]);
+    let mut signal_strength_sum = 0;
     for instruct in input {
         // Break early if all target cycles have been processed.
         if target_cycles.is_empty() {
@@ -90,25 +89,31 @@ fn solve_part1(input: &[Instruction]) -> i64 {
         // Process the instruction
         match instruct {
             Instruction::Noop => {
-                if target_cycles.contains(&clock_cycle) {
-                    output_sum += reg_x * clock_cycle;
-                    target_cycles.remove(&clock_cycle);
-                }
+                // Single cycle for noop
+                add_signal_strength_if_targeted(
+                    &mut target_cycles,
+                    clock_cycle,
+                    &mut signal_strength_sum,
+                    reg_x,
+                );
                 clock_cycle += 1;
             }
-            Instruction::Addv { value } => {
+            Instruction::Addx { value } => {
+                // Two cycles for addx
                 for _ in 0..2 {
-                    if target_cycles.contains(&clock_cycle) {
-                        output_sum += reg_x * clock_cycle;
-                        target_cycles.remove(&clock_cycle);
-                    }
+                    add_signal_strength_if_targeted(
+                        &mut target_cycles,
+                        clock_cycle,
+                        &mut signal_strength_sum,
+                        reg_x,
+                    );
                     clock_cycle += 1;
                 }
                 reg_x += value;
             }
         }
     }
-    output_sum
+    signal_strength_sum
 }
 
 /// Solves AOC 2022 Day 10 Part 2 // Determines the eight capital letters displayed on the CRT
@@ -116,31 +121,24 @@ fn solve_part1(input: &[Instruction]) -> i64 {
 fn solve_part2(input: &[Instruction]) -> String {
     let mut reg_x = 1;
     let mut clock_cycle = 0;
-    let mut output_array: Vec<Vec<char>> = vec![vec!['.'; 40]; 6];
+    // Output array (bool) where false represents dark pixel and true represents lit pixel
+    let mut output_array: [[bool; 40]; 6] = [[false; 40]; 6];
     for instruct in input {
+        // Break when the whole CRT screen has been rendered
         if clock_cycle > 240 {
             break;
         }
+        // Process the instruction
         match instruct {
             Instruction::Noop => {
-                let target_set: HashSet<i64> =
-                    ((reg_x - 1)..=(reg_x + 1)).collect::<HashSet<i64>>();
-                if target_set.contains(&(clock_cycle % 40)) {
-                    let x = (clock_cycle as usize) % 40;
-                    let y = (clock_cycle as usize) / 40;
-                    output_array[y][x] = '#';
-                }
+                // Single cycle for noop
+                draw_pixel_if_selected(reg_x, clock_cycle, &mut output_array);
                 clock_cycle += 1
             }
-            Instruction::Addv { value } => {
+            Instruction::Addx { value } => {
+                // Two cycles for addx
                 for _ in 0..2 {
-                    let target_set: HashSet<i64> =
-                        ((reg_x - 1)..=(reg_x + 1)).collect::<HashSet<i64>>();
-                    if target_set.contains(&(clock_cycle % 40)) {
-                        let x = (clock_cycle as usize) % 40;
-                        let y = (clock_cycle as usize) / 40;
-                        output_array[y][x] = '#';
-                    }
+                    draw_pixel_if_selected(reg_x, clock_cycle, &mut output_array);
                     clock_cycle += 1
                 }
                 reg_x += value;
@@ -151,8 +149,34 @@ fn solve_part2(input: &[Instruction]) -> String {
     decode_output_array(&output_array)
 }
 
+/// Checks if a target clock cycle has been reached. If so, it is removed from the set of the target
+/// clock cycles and the current signal strength (product of clock cycle ordinal position and reg_x)
+/// is added to the output sum.
+fn add_signal_strength_if_targeted(
+    target_cycles: &mut HashSet<i64>,
+    clock_cycle: i64,
+    signal_strength_sum: &mut i64,
+    reg_x: i64,
+) {
+    if target_cycles.contains(&clock_cycle) {
+        *signal_strength_sum += reg_x * (clock_cycle + 1);
+        target_cycles.remove(&clock_cycle);
+    }
+}
+
+/// Checks if the pixel denoted by the current clock cycle is to be drawn (if the three-pixel
+/// sprite located by the reg_x value overlaps with the pixel denoted by the current clock cycle).
+/// If so, the corresponding pixel location in the output array is set to be true.
+fn draw_pixel_if_selected(reg_x: i64, clock_cycle: i64, output_array: &mut [[bool; 40]; 6]) {
+    if ((reg_x - 1)..=(reg_x + 1)).contains(&(clock_cycle % 40)) {
+        let x = (clock_cycle as usize) % 40;
+        let y = (clock_cycle as usize) / 40;
+        output_array[y][x] = true;
+    }
+}
+
 /// Decodes the output array by determining the eight capital letters represented in the array.
-fn decode_output_array(output_array: &[Vec<char>]) -> String {
+fn decode_output_array(output_array: &[[bool; 40]; 6]) -> String {
     // Output
     let mut output = String::new();
     // Letter sequences (6 rows of 5 chars all concatenated for each letter)
@@ -184,18 +208,28 @@ fn decode_output_array(output_array: &[Vec<char>]) -> String {
         ("#...#.#.#...#....#....#....#..", 'Y'),
         ("####....#...#...#...#....####.", 'Z'),
     ]);
-    // Construct output
+    // Construct output for each of the eight character boxes (6px high and 5px wide)
     for i in 0..8 {
         let mut letter_key = String::new();
         for row in output_array {
             for x in 0..5 {
-                letter_key.push(row[x + i * 5]);
+                // Determine the char representation of the pixel from bool array
+                let pixel = {
+                    if row[x + i * 5] {
+                        '#'
+                    } else {
+                        '.'
+                    }
+                };
+                // Push the pixel char to the end of the letter key
+                letter_key.push(pixel);
             }
         }
+        // Determine which capital letter is represented in the current character box
         if letters.contains_key(letter_key.as_str()) {
             output.push(*letters.get(letter_key.as_str()).unwrap());
         } else {
-            output.push('#'); // Placeholder for invalid char
+            output.push('#'); // Placeholder for invalid letter representation
         }
     }
     // Return the result string
