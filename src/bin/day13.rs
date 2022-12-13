49f5ae8e65e5e3a5bac3dabdd8364cdae67f5862
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-12-13 21:38:41 +1000

    Add day13 part1 progress - Day13 part1 solution works for test case but not for problem input   (provides 6047, which is too low)

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,8 +1,11 @@
 use std::fs;
 use std::time::Instant;
 
-const PROBLEM_NAME: &str = "###";
+use regex::Regex;
+
+const PROBLEM_NAME: &str = "Distress Signal";
 const PROBLEM_INPUT_FILE: &str = "./input/day13.txt";
+// const PROBLEM_INPUT_FILE: &str = "./input/test/day13_t001.txt";
 const PROBLEM_DAY: u64 = 13;
 
 /// Processes the AOC 2022 Day 13 input file and solves both parts of the problem. Solutions are
@@ -39,22 +42,90 @@ pub fn main() {
 }
 
 /// Processes the AOC 2022 Day 13 input file in the format required by the solver functions.
-/// Returned value is ###.
-fn process_input_file(filename: &str) -> String {
+/// Returned value is vector of string pairs given in the input file.
+fn process_input_file(filename: &str) -> Vec<(String, String)> {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    let mut output: Vec<(String, String)> = vec![];
+    for pair in raw_input.trim().split("\n\n") {
+        let pair_strings = pair.lines().map(|line| line).collect::<Vec<&str>>();
+        output.push((pair_strings[0].to_string(), pair_strings[1].to_string()));
+    }
+    output
 }
 
 /// Solves AOC 2022 Day 13 Part 1 // ###
-fn solve_part1(_input: &String) -> String {
-    unimplemented!();
+fn solve_part1(input: &[(String, String)]) -> usize {
+    let mut index_sum = 0;
+    let regex_line = Regex::new(r"(\[|\]|\d+)").unwrap();
+    for (i, (left, right)) in input.iter().enumerate() {
+        let left_elems = regex_line
+            .find_iter(left)
+            .map(|elem| elem.as_str().to_string())
+            .collect::<Vec<String>>();
+        let right_elems = regex_line
+            .find_iter(right)
+            .map(|elem| elem.as_str().to_string())
+            .collect::<Vec<String>>();
+        if compare_left_and_right_string(&left_elems, &right_elems) {
+            index_sum += i + 1;
+        }
+    }
+    index_sum
 }
 
 /// Solves AOC 2022 Day 13 Part 2 // ###
-fn solve_part2(_input: &String) -> String {
-    unimplemented!();
+fn solve_part2(_input: &[(String, String)]) -> usize {
+    0
+}
+
+fn compare_left_and_right_string(left_elems: &Vec<String>, right_elems: &Vec<String>) -> bool {
+    // Initialise cursors
+    let mut c_left: usize = 0;
+    let mut c_right: usize = 0;
+    let mut left = left_elems.clone();
+    let mut right = right_elems.clone();
+    loop {
+        // Move out of end of list element
+        while c_left < left.len() && left[c_left] == "]" {
+            c_left += 1;
+        }
+        while c_right < right.len() && right[c_right] == "]" {
+            c_right += 1;
+        }
+        // Check bounds
+        if c_left >= left.len() {
+            // left runs out of elements first
+            return true;
+        } else if c_right >= right.len() {
+            // right runs out of elements first
+            return false;
+        }
+        // Check if both current elements are integers
+        let left_num = left[c_left].parse::<u64>();
+        let right_num = right[c_right].parse::<u64>();
+        if left_num.is_ok() && right_num.is_ok() {
+            let left_num = left_num.unwrap();
+            let right_num = right_num.unwrap();
+            if left_num < right_num {
+                return true;
+            } else if left_num > right_num {
+                return false;
+            }
+        } else if left_num.is_err() && right_num.is_err() {
+            // both items are lists
+            ()
+        } else if right_num.is_ok() {
+            right.insert(c_right, String::from("["));
+            right.insert(c_right + 2, String::from("]"));
+        } else {
+            left.insert(c_left, String::from("["));
+            left.insert(c_left + 2, String::from("]"));
+        }
+        c_left += 1;
+        c_right += 1;
+    }
 }
 
 #[cfg(test)]
