a8699ed5b8276398c33082d937c17ba1fece428a
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-12-17 20:37:05 +1000

    Update day16 - tidy-up and use Rc<> for small part2 speed increase

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs;
+use std::rc::Rc;
 use std::time::Instant;
 
 use regex::Regex;
@@ -8,8 +9,14 @@ const PROBLEM_NAME: &str = "Proboscidea Volcanium";
 const PROBLEM_INPUT_FILE: &str = "./input/day16.txt";
 const PROBLEM_DAY: u64 = 16;
 
-const PART1_MINUTES: u64 = 30;
-const PART2_MINUTES: u64 = 26;
+const PART1_MINUTES: u64 = 30; // allowed 30 minutes for Part 1
+const PART2_MINUTES: u64 = 26; // allowed 26 minutes for Part 2
+
+/// Type definition to simplify function signatures.
+type ProblemInput = (
+    HashMap<Rc<String>, u64>,
+    HashMap<Rc<String>, Vec<Rc<String>>>,
+);
 
 /// Processes the AOC 2022 Day 16 input file and solves both parts of the problem. Solutions are
 /// printed to stdout.
@@ -46,7 +53,7 @@ pub fn main() {
 
 /// Processes the AOC 2022 Day 16 input file in the format required by the solver functions.
 /// Returned value is tuple containing hashmaps with the valve flow rates and valve connections.
-fn process_input_file(filename: &str) -> (HashMap<String, u64>, HashMap<String, Vec<String>>) {
+fn process_input_file(filename: &str) -> ProblemInput {
     // Read contents of problem input file
     let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
@@ -54,8 +61,8 @@ fn process_input_file(filename: &str) -> (HashMap<String, u64>, HashMap<String, 
         r"^Valve ([A-Z]{2}) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.*)$",
     )
     .unwrap();
-    let mut valve_flow_rates: HashMap<String, u64> = HashMap::new();
-    let mut valve_connections: HashMap<String, Vec<String>> = HashMap::new();
+    let mut valve_flow_rates: HashMap<Rc<String>, u64> = HashMap::new();
+    let mut valve_connections: HashMap<Rc<String>, Vec<Rc<String>>> = HashMap::new();
     for line in raw_input.lines() {
         // Trim input line and ignore empty line
         let line = line.trim();
@@ -64,22 +71,22 @@ fn process_input_file(filename: &str) -> (HashMap<String, u64>, HashMap<String, 
         }
         // Extract field data from input line
         let caps = regex_line.captures(line).unwrap();
-        let valve = &caps[1];
+        let valve = Rc::new(String::from(&caps[1]));
         let flow_rate = caps[2].parse::<u64>().unwrap();
         let connections = caps[3]
             .split(", ")
-            .map(|elem| elem.to_string())
-            .collect::<Vec<String>>();
-        valve_flow_rates.insert(valve.to_string(), flow_rate);
-        valve_connections.insert(valve.to_string(), connections);
+            .map(|elem| Rc::new(elem.to_string()))
+            .collect::<Vec<Rc<String>>>();
+        valve_flow_rates.insert(valve.clone(), flow_rate);
+        valve_connections.insert(valve.clone(), connections);
     }
     (valve_flow_rates, valve_connections)
 }
 
 /// Solves AOC 2022 Day 16 Part 1 // Gets the maximum pressure that can be released by opening
 /// valves in the volcano over 30 minutes.
-fn solve_part1(input: &(HashMap<String, u64>, HashMap<String, Vec<String>>)) -> u64 {
-    // Calculate the valve activation times from 
+fn solve_part1(input: &ProblemInput) -> u64 {
+    // Calculate the valve activation times
     let (valve_flow_rates, valve_connections) = input;
     let valve_activation_times = &get_valve_activation_times(valve_flow_rates, valve_connections);
     let possible_paths = determine_possible_paths("AA", valve_activation_times, PART1_MINUTES);
@@ -98,12 +105,52 @@ fn solve_part1(input: &(HashMap<String, u64>, HashMap<String, Vec<String>>)) -> 
     max_pressure_released
 }
 
+/// Solves AOC 2022 Day 16 Part 2 // Gets the maximum pressures that can be released by opening
+/// valves alongside the elephant over 26 minutes.
+fn solve_part2(input: &ProblemInput) -> u64 {
+    let (valve_flow_rates, valve_connections) = input;
+    let valve_activation_times = &get_valve_activation_times(valve_flow_rates, valve_connections);
+    // Find the protagonist paths
+    let possible_paths = determine_possible_paths("AA", valve_activation_times, PART2_MINUTES);
+    let mut maximum_pressure_released = 0;
+    for protagonist_path in possible_paths.iter() {
+        // Find the paths the elephant could take for a given protagonist path
+        let elephant_paths = get_elephant_paths(
+            "AA",
+            protagonist_path,
+            valve_activation_times,
+            PART2_MINUTES,
+        );
+        for ele_path in elephant_paths.iter() {
+            let mut pressure_released = 0;
+            // Calculate the pressure released over the allowed time by the protagonist and elephant
+            pressure_released += get_pressure_released_for_path(
+                protagonist_path,
+                valve_flow_rates,
+                valve_activation_times,
+                PART2_MINUTES,
+            );
+            pressure_released += get_pressure_released_for_path(
+                ele_path,
+                valve_flow_rates,
+                valve_activation_times,
+                PART2_MINUTES,
+            );
+            // Check if a new maximum pressure released value has been found
+            if pressure_released > maximum_pressure_released {
+                maximum_pressure_released = pressure_released;
+            }
+        }
+    }
+    maximum_pressure_released
+}
+
 /// Determines the amount of pressure released over the allowed time by following the given path.
 /// The time required to move to and activate a valve is provided as parameter to this function.
 fn get_pressure_released_for_path(
-    path: &Vec<String>,
-    valve_flow_rates: &HashMap<String, u64>,
-    valve_activation_times: &HashMap<String, HashMap<String, u64>>,
+    path: &Vec<Rc<String>>,
+    valve_flow_rates: &HashMap<Rc<String>, u64>,
+    valve_activation_times: &HashMap<Rc<String>, HashMap<Rc<String>, u64>>,
     minutes_allowed: u64,
 ) -> u64 {
     let mut minutes_remaining = minutes_allowed;
@@ -130,95 +177,53 @@ fn get_pressure_released_for_path(
     total_pressure_released
 }
 
-/// Solves AOC 2022 Day 16 Part 2 // Gets the maximum pressures that can be released by opening
-/// valves alongside the elephant over 26 minutes.
-fn solve_part2(input: &(HashMap<String, u64>, HashMap<String, Vec<String>>)) -> u64 {
-    let (valve_flow_rates, valve_connections) = input;
-    let valve_activation_times = &get_valve_activation_times(valve_flow_rates, valve_connections);
-    // Find the protagonist paths
-    let possible_paths = determine_possible_paths("AA", valve_activation_times, PART2_MINUTES);
-    let mut maximum_pressure_released = 0;
-    // testing vars
-    let mut protag_path_count = 0;
-    for protagonist_path in possible_paths.iter() {
-        // testing vars
-        protag_path_count += 1;
-        if protag_path_count % 100 == 0 {
-            println!("[+] Checking protagonist path {} / {}", protag_path_count, possible_paths.len());
-        }
-        // Find the paths the elephant could take for a given protagonist path
-        let elephant_paths = get_elephant_paths("AA", protagonist_path, valve_activation_times, PART2_MINUTES);
-        for ele_path in elephant_paths.iter() {
-            let mut pressure_released = 0;
-            // Calculate the pressure released over the allowed time by the protagonist and elephant
-            pressure_released += get_pressure_released_for_path(
-                protagonist_path,
-                valve_flow_rates,
-                valve_activation_times,
-                PART2_MINUTES,
-            );
-            pressure_released += get_pressure_released_for_path(
-                ele_path,
-                valve_flow_rates,
-                valve_activation_times,
-                PART2_MINUTES,
-            );
-            // Check if a new maximum pressure released value has been found
-            if pressure_released > maximum_pressure_released {
-                maximum_pressure_released = pressure_released;
-            }
-        }
-    }
-    maximum_pressure_released
-}
-
 /// Gets the time required to move from a valve with a non-zero flow rate (or the start valve "AA")
 /// to another valve with flow.
 fn get_valve_activation_times(
-    valve_flow_rates: &HashMap<String, u64>,
-    valve_connections: &HashMap<String, Vec<String>>,
-) -> HashMap<String, HashMap<String, u64>> {
-    let mut output: HashMap<String, HashMap<String, u64>> = HashMap::new();
+    valve_flow_rates: &HashMap<Rc<String>, u64>,
+    valve_connections: &HashMap<Rc<String>, Vec<Rc<String>>>,
+) -> HashMap<Rc<String>, HashMap<Rc<String>, u64>> {
+    let mut output: HashMap<Rc<String>, HashMap<Rc<String>, u64>> = HashMap::new();
     // Determine the valves to include in the activation time map
-    let mut valid_valves: HashSet<String> = HashSet::new();
-    valid_valves.insert(String::from("AA"));
+    let mut valid_valves: HashSet<Rc<String>> = HashSet::new();
+    valid_valves.insert(Rc::new(String::from("AA")));
     for (valve, flow_rate) in valve_flow_rates.iter() {
         if *flow_rate == 0 {
             continue;
         }
-        valid_valves.insert(valve.to_string());
+        valid_valves.insert(valve.clone());
     }
     // Find the activation times for other valid valves for each valid valve
     for valve in valid_valves.iter() {
         let valve_activation_times =
             get_activation_times_from_start_valve(valve, valve_connections, &valid_valves);
-        output.insert(valve.to_string(), valve_activation_times);
+        output.insert(valve.clone(), valve_activation_times);
     }
     output
 }
 
 /// Gets the times required to move to and activate the valid valves from the given start valve.
 fn get_activation_times_from_start_valve(
-    start_valve: &str,
-    valve_connections: &HashMap<String, Vec<String>>,
-    valid_valves: &HashSet<String>,
-) -> HashMap<String, u64> {
-    let mut visit_queue: VecDeque<(u64, String)> = VecDeque::new();
-    let mut visited: HashSet<String> = HashSet::new();
-    let mut output: HashMap<String, u64> = HashMap::new();
-    visit_queue.push_back((0, start_valve.to_string()));
-    visited.insert(start_valve.to_string());
+    start_valve: &Rc<String>,
+    valve_connections: &HashMap<Rc<String>, Vec<Rc<String>>>,
+    valid_valves: &HashSet<Rc<String>>,
+) -> HashMap<Rc<String>, u64> {
+    let mut visit_queue: VecDeque<(u64, Rc<String>)> = VecDeque::new();
+    let mut visited: HashSet<Rc<String>> = HashSet::new();
+    let mut output: HashMap<Rc<String>, u64> = HashMap::new();
+    visit_queue.push_back((0, start_valve.clone()));
+    visited.insert(start_valve.clone());
     while !visit_queue.is_empty() {
         // Get next valve to visit
         let (steps, valve) = visit_queue.pop_front().unwrap();
-        if valid_valves.contains(valve.as_str()) {
-            output.insert(valve.to_string(), steps + 1);
+        if valid_valves.contains(&valve) {
+            output.insert(valve.clone(), steps + 1);
         }
         // Get next nodes to visit
         for next_valve in valve_connections.get(&valve).unwrap() {
             if !visited.contains(next_valve) {
-                visited.insert(next_valve.to_string());
-                visit_queue.push_back((steps + 1, next_valve.to_string()));
+                visited.insert(next_valve.clone());
+                visit_queue.push_back((steps + 1, next_valve.clone()));
             }
         }
     }
@@ -229,12 +234,12 @@ fn get_activation_times_from_start_valve(
 /// valve.
 fn determine_possible_paths(
     start_valve: &str,
-    valve_activation_times: &HashMap<String, HashMap<String, u64>>,
+    valve_activation_times: &HashMap<Rc<String>, HashMap<Rc<String>, u64>>,
     minutes_allowed: u64,
-) -> Vec<Vec<String>> {
-    let mut possible_paths: Vec<Vec<String>> = vec![];
+) -> Vec<Vec<Rc<String>>> {
+    let mut possible_paths: Vec<Vec<Rc<String>>> = vec![];
     determine_possible_paths_recursive(
-        vec![String::from(start_valve)],
+        vec![Rc::new(String::from(start_valve))],
         minutes_allowed,
         &mut possible_paths,
         valve_activation_times,
@@ -244,10 +249,10 @@ fn determine_possible_paths(
 
 /// Recursive helper function to find possible paths by building from the current path.
 fn determine_possible_paths_recursive(
-    current_path: Vec<String>,
+    current_path: Vec<Rc<String>>,
     time_remaining: u64,
-    possible_paths: &mut Vec<Vec<String>>,
-    valve_activation_times: &HashMap<String, HashMap<String, u64>>,
+    possible_paths: &mut Vec<Vec<Rc<String>>>,
+    valve_activation_times: &HashMap<Rc<String>, HashMap<Rc<String>, u64>>,
 ) {
     let current_valve = current_path.last().unwrap();
     for next_valve in valve_activation_times.keys() {
@@ -263,7 +268,7 @@ fn determine_possible_paths_recursive(
         }
         // Form the new path
         let mut new_path = current_path.clone();
-        new_path.push(next_valve.to_string());
+        new_path.push(next_valve.clone());
         // Keep finding new paths
         determine_possible_paths_recursive(
             new_path,
@@ -279,14 +284,14 @@ fn determine_possible_paths_recursive(
 /// by the protagonist.
 fn get_elephant_paths(
     start_valve: &str,
-    protagonist_path: &Vec<String>,
-    valve_activation_times: &HashMap<String, HashMap<String, u64>>,
+    protagonist_path: &Vec<Rc<String>>,
+    valve_activation_times: &HashMap<Rc<String>, HashMap<Rc<String>, u64>>,
     minutes_allowed: u64,
-) -> Vec<Vec<String>> {
-    let mut elephant_paths: Vec<Vec<String>> = vec![];
+) -> Vec<Vec<Rc<String>>> {
+    let mut elephant_paths: Vec<Vec<Rc<String>>> = vec![];
     get_elephant_paths_recursive(
         protagonist_path,
-        vec![String::from(start_valve)],
+        vec![Rc::new(String::from(start_valve))],
         minutes_allowed,
         &mut elephant_paths,
         valve_activation_times,
@@ -296,11 +301,11 @@ fn get_elephant_paths(
 
 /// Recursive helper function to find the possible elephant paths for a given protagonist path.
 fn get_elephant_paths_recursive(
-    protagonist_path: &Vec<String>,
-    current_path: Vec<String>,
+    protagonist_path: &Vec<Rc<String>>,
+    current_path: Vec<Rc<String>>,
     time_remaining: u64,
-    possible_paths: &mut Vec<Vec<String>>,
-    valve_activation_times: &HashMap<String, HashMap<String, u64>>,
+    possible_paths: &mut Vec<Vec<Rc<String>>>,
+    valve_activation_times: &HashMap<Rc<String>, HashMap<Rc<String>, u64>>,
 ) {
     // Get reference to the current valve - the last valve of the current path
     let current_valve = current_path.last().unwrap();
@@ -320,7 +325,7 @@ fn get_elephant_paths_recursive(
         }
         // Form the new path
         let mut new_path = current_path.clone();
-        new_path.push(next_valve.to_string());
+        new_path.push(next_valve.clone());
         // Keep building elephant paths
         get_elephant_paths_recursive(
             protagonist_path,
