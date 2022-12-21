d924cb162602a73149b7b706247ae494002b1ea8
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-12-21 21:40:25 +1000

    Solve Day 18 Part 1

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,5 +1,8 @@
+use std::collections::HashSet;
 use std::fs;
 use std::time::Instant;
+
+use aoc2022::utils::cartography::Point3D;
 
 const PROBLEM_NAME: &str = "Boiling Boulders";
 const PROBLEM_INPUT_FILE: &str = "./input/day18.txt";
@@ -39,22 +42,70 @@ pub fn main() {
 }
 
 /// Processes the AOC 2022 Day 18 input file in the format required by the solver functions.
-/// Returned value is ###.
-fn process_input_file(filename: &str) -> String {
+/// Returned value is vector of Point3D structs using the co-ordinates listed in the input file.
+fn process_input_file(filename: &str) -> Vec<Point3D> {
     // Read contents of problem input file
-    let _raw_input = fs::read_to_string(filename).unwrap();
+    let raw_input = fs::read_to_string(filename).unwrap();
     // Process input file contents into data structure
-    unimplemented!();
+    let mut output: Vec<Point3D> = vec![];
+    for line in raw_input.lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        let coords = line
+            .split(",")
+            .map(|elem| elem.parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
+        output.push(Point3D::new(coords[0], coords[1], coords[2]));
+    }
+    output
 }
 
-/// Solves AOC 2022 Day 18 Part 1 // ###
-fn solve_part1(_input: &String) -> String {
-    unimplemented!();
+/// Solves AOC 2022 Day 18 Part 1 // Determines the surface area of the scanned lava droplet.
+fn solve_part1(cubes: &[Point3D]) -> u64 {
+    let mut observed_cubes: HashSet<Point3D> = HashSet::new();
+    let mut total_surface_area = 0;
+    for cube in cubes {
+        let mut exposed_sides = 6;
+        // dx
+        if observed_cubes.contains(&cube.check_move_point(-1, 0, 0)) {
+            exposed_sides -= 1;
+            total_surface_area -= 1;
+        }
+        if observed_cubes.contains(&cube.check_move_point(1, 0, 0)) {
+            exposed_sides -= 1;
+            total_surface_area -= 1;
+        }
+        // dy
+        if observed_cubes.contains(&cube.check_move_point(0, -1, 0)) {
+            exposed_sides -= 1;
+            total_surface_area -= 1;
+        }
+        if observed_cubes.contains(&cube.check_move_point(0, 1, 0)) {
+            exposed_sides -= 1;
+            total_surface_area -= 1;
+        }
+        // dz
+        if observed_cubes.contains(&cube.check_move_point(0, 0, -1)) {
+            exposed_sides -= 1;
+            total_surface_area -= 1;
+        }
+        if observed_cubes.contains(&cube.check_move_point(0, 0, 1)) {
+            exposed_sides -= 1;
+            total_surface_area -= 1;
+        }
+        // Add exposed sides
+        total_surface_area += exposed_sides;
+        observed_cubes.insert(*cube);
+    }
+    total_surface_area
 }
 
-/// Solves AOC 2022 Day 18 Part 2 // ###
-fn solve_part2(_input: &String) -> String {
-    unimplemented!();
+/// Solves AOC 2022 Day 18 Part 2 // Determines the external surface area of the scanned lava
+/// droplet.
+fn solve_part2(_cubes: &[Point3D]) -> u64 {
+   0
 }
 
 #[cfg(test)]
@@ -65,9 +116,8 @@ mod test {
     #[test]
     fn test_day18_p1_actual() {
         let input = process_input_file(PROBLEM_INPUT_FILE);
-        let _solution = solve_part1(&input);
-        unimplemented!();
-        // assert_eq!("###", solution);
+        let solution = solve_part1(&input);
+        assert_eq!(4332, solution);
     }
 
     /// Tests the Day 18 Part 2 solver method against the actual problem solution.
